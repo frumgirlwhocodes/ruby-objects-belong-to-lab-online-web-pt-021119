0227510afaa7b62cb711439b14607a2b5c07c4f2
lieby weintraub <liebahinda@gmail.com>
2019-03-18 14:54:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,16 @@
   class Song
  
   attr_accessor :title, :artist
+  
+  def initialize 
+    title=@title
+    artist=@artist 
+  end 
  
   
-def artist=(artist)
-     artist.is_a? Artist
-      @artist = artist
-    end 
-  end
+#def artist=(artist)
+   #  artist.is_a? Artist
+   #   @artist = artist
+  #  end 
+ # end
  
